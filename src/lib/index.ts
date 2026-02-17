1aab1393dee200372649144a21d5d726b7c7b352
DrOctoPunk <sales@octopunk.io>
2026-02-17 11:44:02 -0500

    Add src/lib/index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,5 @@
+export { default as DataTable } from './DataTable.svelte';
+export { default as TableHeader } from './TableHeader.svelte';
+export { default as TableBody } from './TableBody.svelte';
+export { default as TablePagination } from './TablePagination.svelte';
+export type { Column, SortState, SelectionState, PaginationConfig } from './types';
